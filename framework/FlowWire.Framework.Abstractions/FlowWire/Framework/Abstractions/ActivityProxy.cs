2a6d807b43c2f1482efdaf4bb9470f4c53d769c3
Mathias Døhl <fckmathias@hotmail.com>
2026-01-06 14:05:08 +0100

    Add ActivityProxy struct for activity scheduling
    Introduces the ActivityProxy<T> struct with methods to schedule activities using expression trees. This provides a type-safe way to define and run workflow activities.

---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/ActivityProxy.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace FlowWire.Framework.Abstractions;
+
+public struct ActivityProxy<T>
+{
+    public readonly WorkflowCommand Run(Expression<Func<T, Task>> action)
+    {
+
+        return new Internal.ScheduleActivityFromExpression(action);
+    }
+
+    public readonly WorkflowCommand Run<TResult>(Expression<Func<T, Task<TResult>>> action)
+    {
+        return new Internal.ScheduleActivityFromExpression(action);
+    }
+}
