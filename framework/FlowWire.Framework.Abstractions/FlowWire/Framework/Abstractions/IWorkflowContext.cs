65364d4325d814821f9fab233b42392e284e3807
Mathias Døhl <fckmathias@hotmail.com>
2026-01-06 14:04:42 +0100

    Add IWorkflowContext interface definition

---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/IWorkflowContext.cs
@@ -0,0 +1,31 @@
+﻿namespace FlowWire.Framework.Abstractions;
+
+internal interface IWorkflowContext
+{
+    /// <summary>
+    /// The unique ID of this workflow instance.
+    /// </summary>
+    string WorkflowId { get; }
+
+    /// <summary>
+    /// Current deterministic time. 
+    /// Constant during a single Flow() tick.
+    /// </summary>
+    DateTimeOffset CurrentUtc { get; }
+
+    /// <summary>
+    /// A deterministic random number generator seeded by (WorkflowID + Tick).
+    /// </summary>
+    Random Random { get; }
+
+    /// <summary>
+    /// The current version of the executed logic (useful for migration checks).
+    /// </summary>
+    int CurrentTick { get; }
+
+    /// <summary>
+    /// Check if we are currently replaying history
+    /// or simply providing context on if this is a "Dry Run" (Shadow Mode).
+    /// </summary>
+    bool IsShadowMode { get; }
+}
