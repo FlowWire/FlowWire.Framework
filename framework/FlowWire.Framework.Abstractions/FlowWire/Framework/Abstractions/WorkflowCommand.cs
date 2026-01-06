4c731f0c0fb2f429db2c3259913fbca3d3d6c010
Mathias Døhl <fckmathias@hotmail.com>
2026-01-06 14:05:23 +0100

    Add abstract WorkflowCommand record
    Introduces the WorkflowCommand abstract record as the base unit of work for flows, to be used with FlowAttribute.

---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/WorkflowCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace FlowWire.Framework.Abstractions;
+
+/// <summary>
+/// The base unit of work returned by a <see cref="FlowAttribute" />.
+/// </summary>
+public abstract record WorkflowCommand;
