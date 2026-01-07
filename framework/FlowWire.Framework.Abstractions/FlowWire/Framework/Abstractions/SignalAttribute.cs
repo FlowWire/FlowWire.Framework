91a568ca1b4feac40cd45ea655073dbb5d8a33ce
Mathias Døhl <fckmathias@hotmail.com>
2026-01-07 14:15:55 +0100

    Add SignalAttribute for marking signal handler methods

---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/SignalAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace FlowWire.Framework.Abstractions;
+
+/// <summary>
+/// Marks a method as a Signal Handler.
+/// </summary>
+/// <param name="name">Optional override for the signal name.</param>
+[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+public sealed class SignalAttribute(string? name = null) : Attribute
+{
+    /// <summary>
+    /// The public name of the signal (e.g., "Approve").
+    /// If null, the method name is used.
+    /// </summary>
+    public string? Name { get; } = name;
+}
