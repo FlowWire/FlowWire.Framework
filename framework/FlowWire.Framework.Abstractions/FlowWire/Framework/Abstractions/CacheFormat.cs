e28b7b1434d6389e25a8d033a398ce31d6c73e21
Mathias Døhl <fckmathias@hotmail.com>
2026-01-07 16:21:30 +0100

    Remove unused using directives in CacheFormat.cs

--- framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/CacheFormat.cs
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/CacheFormat.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FlowWire.Framework.Abstractions;
+﻿namespace FlowWire.Framework.Abstractions;
 
 public enum CacheFormat
 {
