f4b61d89549caf9094bc70e4106b0d736ef7f893
Mathias Døhl <fckmathias@hotmail.com>
2026-01-08 11:17:50 +0100

    Add FlowWireBuilder and DI abstractions
    Introduces FlowWireBuilder and IFlowWireBuilder for dependency injection configuration. Adds Microsoft.Extensions.DependencyInjection.Abstractions and Hosting.Abstractions package references to support DI patterns in FlowWire.Framework.Core.

---
+++ framework/FlowWire.Framework.Core/FlowWire/Framework/Core/Configuration/FlowWireBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowWire.Framework.Core.Configuration;
+
+internal class FlowWireBuilder(IServiceCollection services) : IFlowWireBuilder
+{
+    public IServiceCollection Services { get; } = services;
+}
---
+++ framework/FlowWire.Framework.Core/FlowWire/Framework/Core/Configuration/IFlowWireBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowWire.Framework.Core.Configuration;
+
+public interface IFlowWireBuilder
+{
+    /// <summary>
+    /// The underlying service collection. 
+    /// Used to register dependencies into the DI container.
+    /// </summary>
+    IServiceCollection Services { get; }
+}
