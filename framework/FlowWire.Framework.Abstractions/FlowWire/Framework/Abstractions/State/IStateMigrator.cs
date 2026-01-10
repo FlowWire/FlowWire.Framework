4d9426d7a0a35ce549e785da7365fd5312b675b6
Mathias Døhl <fckmathias@hotmail.com>
2026-01-10 21:52:58 +0100

    Add IStateMigrator interface for state schema migration

---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/State/IStateMigrator.cs
@@ -0,0 +1,16 @@
+﻿namespace FlowWire.Framework.Abstractions.State;
+
+/// <summary>
+/// Defines a pipeline for upgrading state schemas from version N to Current.
+/// </summary>
+public interface IStateMigrator
+{
+    /// <summary>
+    /// Migrates the state object from the stored version to the target version.
+    /// </summary>
+    /// <param name="state">The deserialized state (potentially an old version DTO).</param>
+    /// <param name="fromVersion">The version found in the binary header.</param>
+    /// <param name="toVersion">The current version of the [FlowState].</param>
+    /// <returns>The migrated state object matching the current schema.</returns>
+    object Migrate(object state, int fromVersion, int toVersion);
+}
