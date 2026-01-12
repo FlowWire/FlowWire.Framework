5590beecf39eb7ae383919c79cd0d6f50fb9d841
Mathias Døhl <fckmathias@hotmail.com>
2026-01-12 21:18:33 +0100

    Add FlowMode

--- framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/FlowAttribute.cs
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/FlowAttribute.cs
@@ -1,4 +1,11 @@
 ﻿namespace FlowWire.Framework.Abstractions;
 
-[AttributeUsage(AttributeTargets.Class, Inherited = false)]
-public class FlowAttribute : Attribute { }
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public class FlowAttribute : Attribute
+{
+    /// <summary>
+    /// Defines the execution mode (Lifecycle Policy) for this Flow.
+    /// Default is <see cref="FlowMode.Memory"/>.
+    /// </summary>
+    public FlowMode Mode { get; set; } = FlowMode.Memory;
+}
---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/FlowMode.cs
@@ -0,0 +1,35 @@
+﻿namespace FlowWire.Framework.Abstractions;
+
+/// <summary>
+/// Defines the lifecycle and creation policy of a Flow.
+/// </summary>
+public enum FlowMode
+{
+    /// <summary>
+    /// <b>(Default)</b> The Flow acts as a <b>Memory</b>.
+    /// <br/>
+    /// <b>Metaphor:</b> A passive memory bank.
+    /// <br/>
+    /// <b>Behavior:</b>
+    /// <list type="bullet">
+    /// <item>It is purely "Reactive" (Loose). Any Impulse will upsert the state.</item>
+    /// <item>It implies the Flow is primarily a State Container (Entity).</item>
+    /// <item>Perfect for Digital Twins, Shopping Carts, and IoT Shadows.</item>
+    /// </list>
+    /// </summary>
+    Memory = 0,
+
+    /// <summary>
+    /// The Flow acts as a <b>Circuit</b>.
+    /// <br/>
+    /// <b>Metaphor:</b> An active logical network.
+    /// <br/>
+    /// <b>Behavior:</b>
+    /// <list type="bullet">
+    /// <item>It is "Gated" (Strict). It requires a specific startup signal (<c>IsInit</c>) to energize.</item>
+    /// <item>It implies the Flow has a complex Lifecycle or Process.</item>
+    /// <item>Perfect for Orders, Payments, and Workflows.</item>
+    /// </list>
+    /// </summary>
+    Circuit = 1
+}
