0546b35e2da8dc4196158fef15753df786c5ccd5
Mathias Døhl <fckmathias@hotmail.com>
2026-01-12 21:18:13 +0100

    Add GeneratedFlowConfigurationAttribute class

---
+++ framework/FlowWire.Framework.Abstractions/FlowWire/Framework/Abstractions/GeneratedFlowConfigurationAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowWire.Framework.Abstractions;
+
+/// <summary>
+/// Applied by the Source Generator to expose compile-time metadata 
+/// to the runtime engine without expensive reflection scanning.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class GeneratedFlowConfigurationAttribute(Type stateType) : Attribute
+{
+    public Type StateType { get; } = stateType;
+}
