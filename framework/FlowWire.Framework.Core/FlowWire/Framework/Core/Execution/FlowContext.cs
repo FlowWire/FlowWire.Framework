ef8fc3b9f07c20f434788cd0799d535709d9a4e2
Mathias Døhl <fckmathias@hotmail.com>
2026-01-14 00:05:36 +0100

    Add FlowContext implementation

---
+++ framework/FlowWire.Framework.Core/FlowWire/Framework/Core/Execution/FlowContext.cs
@@ -0,0 +1,24 @@
+﻿using FlowWire.Framework.Abstractions;
+
+namespace FlowWire.Framework.Core.Execution;
+
+public class FlowContext(string flowId, DateTimeOffset currentUtc, Random random, IServiceProvider services) : IFlowContext
+{
+    public string FlowId { get; } = flowId;
+
+    public DateTimeOffset CurrentUtc { get; } = currentUtc;
+
+    public Random Random { get; } = random;
+
+    public int CurrentTick { get; } = 0;
+
+    public bool IsShadowMode { get; } = false;
+
+    public T GetService<T>()
+    {
+        var service = services.GetService(typeof(T))
+            ?? throw new InvalidOperationException($"Service {typeof(T).Name} not found.");
+
+        return (T)service;
+    }
+}
